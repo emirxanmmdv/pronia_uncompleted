d86f5831b6c8c336a95c88635b1b9b9ac4cd506f
emirxanmmdv <tu7hse18h@code.edu.az>
2023-11-04 00:56:38 +0400

    files modified!

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+const usdbtn = document.querySelector('.usdbtn')
+const submenu = document.querySelector('.submenu')
+usdbtn.addEventListener("click",()=>{
+    submenu.classList.toggle("active");
+    
+
+
+
+
+
+
+
+})
