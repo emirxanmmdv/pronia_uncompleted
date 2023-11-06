0ae75ce9de60a1c7a0f754b3d0632cb4ba0fd79e
emirxanmmdv <tu7hse18h@code.edu.az>
2023-11-06 05:14:12 +0400

    files modified!

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,11 @@
 const usdbtn = document.querySelector('.usdbtn')
 const langbtn = document.querySelector('.langbtn')
-const submenu = document.querySelector('.usd-subMenu')
-const langmenu = document.querySelector('.lang-subMenu')
 
-usdbtn.addEventListener("click",()=>{
-    submenu.classList.toggle("active");
-})
-
-langbtn.addEventListener("click", ()=>{
-    langmenu.classList.toggle("active");
+usdbtn.addEventListener("click", ()=>{
+    
 
 })
+
+
+
+
