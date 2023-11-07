516a6ba25c4a24b42fe21181eb40f5a10ed87ff1
emirxanmmdv <tu7hse18h@code.edu.az>
2023-11-07 08:06:26 +0400

    files modified!

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,36 @@
-const usdbtn = document.querySelector('.usdbtn')
-const langbtn = document.querySelector('.langbtn')
+// ___________________SLICK SLIDER_________________________
 
-usdbtn.addEventListener("click", ()=>{
+
+// const usdbtn = document.querySelector('.usdbtn')
+// const langbtn = document.querySelector('.langbtn')
+
+// usdbtn.addEventListener("click", ()=>{
+
+var splide = new Splide( '.splide', {
+  type   : 'loop',
+  perPage: 3,
+  perMove: 1,
+  fixedWidth : '100%' 
+} );
+
+splide.mount();
+
+// })
+const searchbtn = document.querySelector(".searchbtn")
+searchbtn.addEventListener("click", ()=>{
     
-
 })
+
+
+const searchContent = document.querySelector('.search-content')
+function closeSearchContent() {
+    searchContent.style.display = "none"
+
+}
+function openSearchContent() {
+    searchContent.style.display = "flex"
+    searchContent.style.zIndex = "1"
+}
 
 
 
